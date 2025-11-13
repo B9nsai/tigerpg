a7c38fbb1d17f99f8dac10fa0aeb53ce36e18468
B9nsai <l.stuetz98@gmail.com>
2025-11-13 15:25:06 +0100

    feat: added Card, xp as prop in playerStore

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white shadow-lg rounded-xl p-4 border border-gray-200 hover:shadow-xl transition h-64 w-48" @click="$emit('gain')" >
+  <div class="bg-white shadow-lg rounded-xl p-4 border border-gray-200 hover:shadow-xl transition h-64 w-48" @click="$emit('gain', xp)" >
     <h2 class="text-xl font-bold text-black mb-2">{{ title }}</h2>
     <p class="text-gray-600 mb-4">{{ description }}</p>
     <!-- <button @click="player.gainXp(20) "class="text-gray-600 mb-4">{{ description }}</button> -->
@@ -18,5 +18,10 @@ defineProps({
     required: false,
     default: '',
   },
+  xp: {
+    type: Number,
+    required: false,
+    default: 5
+  }
 })
 </script>
